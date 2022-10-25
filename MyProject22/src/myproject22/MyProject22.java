3d487b9508bbb6561b1e9ae95ca348ebdf3ff95d
DaveSamiaji <89759815+DaveSamiaji@users.noreply.github.com>
2022-10-25 11:33:24 +0700

    Add files via upload

---
+++ MyProject22/src/myproject22/MyProject22.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject22;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class MyProject22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("DDDDD  ");
+        System.out.print("     AAAAA     ");
+        System.out.println("VV        VV");
+        System.out.print("D    DD");
+        System.out.print("    AA   AA  ");
+        System.out.println("   VV      VV");
+        System.out.print("D    DD");
+        System.out.print("   AA AAA AA  ");
+        System.out.println("   VV    VV  ");
+        System.out.print("D    DD");
+        System.out.print("  AA       AA   ");
+        System.out.println("  VV  VV  ");
+        System.out.print("DDDDD");
+        System.out.print("   AA         AA  ");
+        System.out.println("    VV   ");
+      
+    }
+    }
+

270457923c777d2b7b8287556f82e9d377bb4bec
DaveSamiaji <89759815+DaveSamiaji@users.noreply.github.com>
2022-10-25 11:40:28 +0700

    Add files via upload

---
+++ ProjectBangunDatar/src/projectbangundatar/Lingkaran.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Salsabila
+ */
+public class Lingkaran {
+    // atribut
+    
+    int jejari;
+    
+    // methods
+    
+    void hitungLuas(){
+        int luas = (int) (3.14 * this.jejari * this.jejari);
+        System.out.println("Luas: " + luas + " satuan luas");
+    }
+    
+    void hitungKeliling(){
+        int keliling = (int) (2 *  3.14 *  this.jejari);
+        System.out.println("Keliling: " + keliling + " satuan panjang");
+    }
+    
+    void input(){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan panjang : ");
+        this.jejari = input.nextInt();
+        
+        this.hitungLuas();
+        this.hitungKeliling();
+    }
+}
---
+++ ProjectBangunDatar/src/projectbangundatar/PersegiPanjang.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Salsabila
+ */
+public class PersegiPanjang {
+    // atribut
+    
+    int panjang;
+    int lebar;
+    
+    // methods
+    
+    void hitungLuas(){
+        int luas = this.panjang * this.lebar;
+        System.out.println("Luas: " + luas + " satuan luas");
+    }
+    
+    void hitungKeliling(){
+        int keliling = 2 * (this.lebar + this.panjang);
+        System.out.println("Keliling: " + keliling + " satuan panjang");
+    }
+    
+    void input(){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan panjang : ");
+        this.panjang = input.nextInt();
+        
+        System.out.print("Masukkan lebar : ");
+        this.lebar = input.nextInt();
+        
+        this.hitungLuas();
+        this.hitungKeliling();
+    }
+        
+}
